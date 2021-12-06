69a1f851b51e4e9a810b82edfb6db4f80950e649
Amanda Lyvik <amanda.lyvik@gmail.com>
2021-12-06 13:31:08 +0100

    Initial parser test

---
+++ Program.fs
@@ -0,0 +1,43 @@
+﻿
+open FParsec
+open System.IO
+open System.Text
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance)
+
+// -------------------------------------------------------------
+
+let ws = many1Satisfy (function ' '|'\t' -> true | _ -> false)
+
+// Parse a lable
+let identifier : Parser<string, unit> =
+    let isIdentifierFirstChar c = c ='#'
+    let isIdentifierChar c = isUpper c
+
+    many1Satisfy2L isIdentifierFirstChar isIdentifierChar "identifier" .>> ws// the "identifier" is used in error messages
+
+let word = many1Satisfy (function ' '|'\t'|'"' -> false | _ -> true)
+let str = stringsSepBy word ws
+
+let value = 
+    choice [pstring "\"\"" 
+            between (pstring "\"") (pstring "\"") str 
+            word ] .>> ws
+
+let item = identifier .>>. many1 value .>> newline
+
+// run 'item' until eof
+let SIE = many1 item .>> eof
+
+// -------------------------------------------------------------
+
+// Get the file path
+let baseDirectory = __SOURCE_DIRECTORY__
+let fileName = "test.SE"
+let filePath = Path.Combine(baseDirectory, "sample_SIE", fileName)
+
+// Parse from file
+let enc = Encoding.GetEncoding(437)
+let result = runParserOnFile SIE () filePath enc
+             
+System.Console.WriteLine(sprintf "%A" result)
---
+++ interactive.fsx
@@ -0,0 +1,7 @@
+// Does NOT work...
+//#r @"C:\Users\Amanda\Documents\Tautologics\test-parser\bin\FParsecCS.dll"
+//#r @"C:\Users\Amanda\Documents\Tautologics\test-parser\bin\FParsec.dll"
+
+// works if files are in root 
+#r @"FParsecCS.dll"
+#r @"FParsec.dll"
